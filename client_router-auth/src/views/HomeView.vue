438acbf3b844fe7bda849282a5a13db0fda5f5d6
TAO233-JPG <1040581451@qq.com>
2022-12-05 23:01:04 +0800

    client/fix: 调整homeView页面，使home展示主要内容（header，sidebar，page）而不是App组件

--- client_router-auth/src/views/HomeView.vue
+++ client_router-auth/src/views/HomeView.vue
@@ -1,5 +1,20 @@
-<script setup lang="ts"></script>
-
+<script setup lang="ts">
+import HeaderComponent from "../components/HeaderComponent.vue";
+import PageComponent from "../components/PageComponent.vue";
+import SideComponent from "../components/SideComponent.vue";
+</script>
 <template>
-  <main>home</main>
+  <HeaderComponent></HeaderComponent>
+  <div class="container">
+    <SideComponent />
+    <PageComponent />
+  </div>
 </template>
+
+<style>
+.container {
+  display: flex;
+  width: 100%;
+  height: 100%;
+}
+</style>
