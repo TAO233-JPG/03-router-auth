035d459130d8ee338154c141d18d885d8c2cb826
TAO233-JPG <1040581451@qq.com>
2022-12-05 23:06:59 +0800

    client/fix: 删去无效内容

--- client_router-auth/src/server/axios.ts
+++ client_router-auth/src/server/axios.ts
@@ -14,7 +14,6 @@ axios.interceptors.request.use((config) => {
 axios.interceptors.response.use((response) => {
   console.log(`axios 响应拦截器`);
 
-  console.log(response);
   if (response.data.code === 1) {
     return response.data.data;
   }
--- client_router-auth/src/stores/user.ts
+++ client_router-auth/src/stores/user.ts
@@ -1,9 +1,8 @@
-import { reactive, ref } from "vue";
+import { ref } from "vue";
 import { defineStore } from "pinia";
 import type { RouteT } from "@/types/types";
 import { login } from "@/server";
 import { format_route_tree } from "@/utils/utils";
-import { generate_route } from "@/router/routre";
 
 export const useUserStore = defineStore("user", () => {
   const username = ref<string>("");
@@ -17,7 +16,6 @@ export const useUserStore = defineStore("user", () => {
     const lists = await login({ username: username.value });
     const _routeTree = format_route_tree(lists);
     routeTree.value = _routeTree;
-    const routes = generate_route(routeTree.value);
   };
 
   const set_user_auth = function (auth: boolean) {
