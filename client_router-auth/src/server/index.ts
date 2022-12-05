0e22a99f04a0c822d66f06b4096f178c593c25cc
TAO233-JPG <1040581451@qq.com>
2022-12-05 15:47:47 +0800

    client/feat: 添加接口login

--- client_router-auth/src/server/index.ts
+++ client_router-auth/src/server/index.ts
@@ -1,25 +1,12 @@
-import axios from "axios";
+import type { RouteT } from "@/types/types";
+import axios from "./axios";
 
-axios.create({
-  baseURL: "http://127.0.0.1:5173/",
-  timeout: 6 * 1000,
-});
+type LoginParamsT = {
+  username: string;
+};
 
-axios.interceptors.request.use((config) => {
-  console.log(`axios 请求拦截器`);
-
-  return config;
-});
-
-axios.interceptors.response.use((response) => {
-  console.log(`axios 响应拦截器`);
-
-  console.log(response);
-  if (response.data.code === 1) {
-    return response.data.data;
-  }
-
-  return Promise.reject(response.data.data);
-});
-
-export default axios;
+// 登录并获取用户可以访问的路由
+export const login = async (data: LoginParamsT): Promise<RouteT[]> => {
+  const list = await axios.post<any, RouteT[]>("/login", data);
+  return list;
+};
