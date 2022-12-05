c33b8f43367e4efb70f7f14e5ac4f06c391c5896
TAO233-JPG <1040581451@qq.com>
2022-12-05 23:03:40 +0800

    client/feat: 新增组件header，page，side

---
+++ client_router-auth/src/components/HeaderComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>路由鉴权练习</header>
+</template>
+<style scoped>
+header {
+  width: 100%;
+  height: 60px;
+  padding: 0px 20px;
+  line-height: 60px;
+  box-sizing: border-box;
+  font-size: 28px;
+  background-color: bisque;
+}
+</style>
---
+++ client_router-auth/src/components/SideComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul>
+    <li>
+      <router-link to="/">首页</router-link>
+    </li>
+    <li v-for="route of userStore.routeTree" :key="route.path">
+      <MenuItemComponent :route-item="route" />
+    </li>
+  </ul>
+</template>
+<script setup lang="ts">
+import MenuItemComponent from "./MenuItemComponent.vue";
+import { useUserStore } from "@/stores/user";
+const userStore = useUserStore();
+</script>
+<style></style>
