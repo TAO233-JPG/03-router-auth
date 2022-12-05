919460a43fe7680aa983bad800e8decfa47c171f
TAO233-JPG <1040581451@qq.com>
2022-12-05 22:54:19 +0800

    client/fix: 修复format_route_tree中的data_to_tree方法转化数据错误，使routeTree数据不全异常

--- client_router-auth/src/utils/utils.ts
+++ client_router-auth/src/utils/utils.ts
@@ -7,22 +7,19 @@ export const format_route_tree = (data: RouteT[]): RouteT[] => {
   data_to_tree(parents, children);
 
   return parents;
-
-  function data_to_tree(parents: RouteT[], children: RouteT[]) {
-    parents.forEach((parent) => {
-      children.forEach((child, idx) => {
-        if (parent.id === child.pid) {
-          const _children: RouteT[] = JSON.parse(
-            JSON.stringify(children.splice(idx, 1))
-          );
-          data_to_tree([child], _children);
-          if (parent.children) {
-            parent.children.push(child);
-          } else {
-            parent.children = [child];
-          }
-        }
-      });
-    });
-  }
 };
+function data_to_tree(parents: RouteT[], children: RouteT[]) {
+  parents.map((parent) => {
+    children.map((child) => {
+      if (parent.id === child.pid) {
+        const _children: RouteT[] = JSON.parse(JSON.stringify(children));
+        data_to_tree([child], _children);
+        if (parent.children) {
+          parent.children.push(child);
+        } else {
+          parent.children = [child];
+        }
+      }
+    });
+  });
+}
